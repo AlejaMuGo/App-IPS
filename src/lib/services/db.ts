c77caf3c444aabdf8e163c35c950f04b7c021c3d
Aleja Muñoz <mamunozgo@unal.edu.co>
2025-02-02 20:39:56 -0500

    feat: Agregar controlador de base de datos

---
+++ src/lib/services/db.ts
@@ -0,0 +1,21 @@
+import { supabase } from "$lib/services/supabase";
+import type { Client } from "$lib/types/client";
+
+export class DatabaseController {
+
+    async createClient(client: Client): Promise<Client | null> {
+        const { data, error } = await supabase.from('client').insert([client]).select();
+
+        if (data === null || data.length === 0) {
+            return null;
+        }
+
+        return data[0] as Client;
+    }
+
+    async getClientes(): Promise<Client[]> {
+        let { data: clients, error } = await supabase.from('client').select('*');
+        return clients as Client[];
+    }
+
+}
