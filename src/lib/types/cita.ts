9690dd19da4c97092160543c0321ebe851a6593b
Aleja Muñoz <mamunozgo@unal.edu.co>
2025-02-09 21:28:33 -0500

    Feat: Creación tipos cita y service

---
+++ src/lib/types/cita.ts
@@ -0,0 +1,12 @@
+import type { Client } from "./client"
+import type { Profesional } from "./profesional"
+import type { Service } from "./service";
+
+export type Cita = {
+    client: Client;
+    service: Service;
+    profesional: Profesional;
+    date: string;
+    start_time: string;
+    end_time: string;
+}
