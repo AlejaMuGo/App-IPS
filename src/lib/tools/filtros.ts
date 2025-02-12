6e057ee81627bc8041220cb495aa2f3e10becbec
Paula Suarez <pasuarezgu@unal.edu.co>
2025-02-12 14:59:21 -0500

    feat: agregamos tipo y funcion para filtrar una cita por fecha

--- src/lib/tools/filtros.ts
+++ src/lib/tools/filtros.ts
@@ -1,12 +1,47 @@
-import type { Cita } from "$lib/types/cita";
+import type { Cita } from '$lib/types/cita';
+import type { OpcionesFiltroFecha } from '$lib/types/filtros';
 
-export function citaCumpleFiltroEstado(cita: Cita, valorFiltro: "confirmada" | "noconfirmada" | ""): boolean {
-		if (valorFiltro === "") return true;
+export function citaCumpleFiltroEstado(
+	cita: Cita,
+	valorFiltro: 'confirmada' | 'noconfirmada' | ''
+): boolean {
+	if (valorFiltro === '') return true;
 
-		const valueEstado = valorFiltro === "confirmada" ? true : false;
-		if (cita.confirmed === valueEstado) {
-			return true;
+	const valueEstado = valorFiltro === 'confirmada' ? true : false;
+	if (cita.confirmed === valueEstado) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+export function citaCumpleFiltroFecha(cita: Cita, valorfiltro: OpcionesFiltroFecha): boolean {
+	const hoy = new Date();
+	const fechaCita = new Date(cita.date);
+
+	if (valorfiltro === '') return true;
+
+	if (valorfiltro === 'pasados') {
+		if (fechaCita < hoy) {
+            return true;
 		} else {
-			return false;
-		}
+            return false;
+        }
+	}
+    if (valorfiltro === 'proximos') {
+        if (fechaCita > hoy) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    if (valorfiltro === 'hoy') {
+        if (fechaCita.toDateString() === hoy.toDateString()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    return false;
 }
